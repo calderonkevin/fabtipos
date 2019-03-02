f83e8e05330a62d29c6f245235d37c8e6da9a087
ocalderon <calderon_edsel@hotmail.com>
2019-03-02 14:05:34 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent }  from './header/header.component';
 import { ShopComponent }  from './shop/shop.component';
 import { CatalogComponent }  from './catalog/catalog.component';
+import { LoginComponent }  from './login/login.component';
+import { ZnoexisteComponent }  from './znoexiste/znoexiste.component';
 import { ProductService } from './common/services/product.service';
 //material
 import { MaterialModule } from './zmaterial/material';
@@ -19,20 +22,25 @@ import{ AngularFireModule} from 'angularfire2';
 import{ AngularFireDatabase, AngularFireDatabaseModule} from 'angularfire2/database';
 import { environment } from '../environments/environment';
 
+import { routing, appRoutingProviders }  from './app.routing';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, ShopComponent, CatalogComponent
+    AppComponent, HeaderComponent, ShopComponent, CatalogComponent, LoginComponent, ZnoexisteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    routing,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule
   ],
-  providers: [ProductService],
+  providers: [ProductService, appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
