9f3cf73815e11cc302b3b9d674068bb4ecdc5b80
calderonkevin <calderon_kevin@yahoo.es>
2019-03-03 12:07:27 -0500

    Menu izquierdo

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -6,6 +6,10 @@ import { map, startWith } from 'rxjs/operators';
 import { ProductService } from '../common/services/product.service';
 import { AttrAst } from '@angular/compiler';
 import { CloseScrollStrategy } from '@angular/cdk/overlay';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+declare var jQuery: any;
+declare var $: any;
 
 
 export interface Producto {
@@ -37,6 +41,7 @@ export class ShopComponent {
   invoiceCab = [];
   //invoice = [];
   productObj: Producto;
+  searchValue: string = "";
 
   constructor(
     private _productService: ProductService
@@ -109,71 +114,79 @@ export class ShopComponent {
     return total.toFixed(2)
   }
 
-  
-  buscarFire(searchValue: string): void {
-    console.log("hola del boton: " + searchValue);
 
-    
-    
-    var aRef = this._productService.getIdProducto("aaa").list("listabarra/00080300034604019")
+  buscarFire(searchValue: string): void {
+    this.searchValue = $("#detectabarra").val();
+    console.log("ESTO VIENE DEL TEXTO:" + this.searchValue);
+
+
+    var aRef = this._productService.getIdProducto("aaa").list("listabarra/" + this.searchValue)
       .snapshotChanges()
       .subscribe(item => {
         //this.productObj = null;
-        console.log("Item: "+item);
+        console.log("Item: " + item);
         //console.log("SubItem: "+item.payload);
 
         //console.log("Item: "+item.payload.;
         //console.log(item.length);
         //console.log("aaaa: "+item.forEach.);
         var arr = [];
-        arr['serpro'] = '00080300034604019';
+        arr['serpro'] = this.searchValue;
         item.forEach(element => {
           let x = element.payload.toJSON();
           console.log("key:" + element.key);
-          console.log("xxx:" +x);
-          arr[element.key] = x; 
+          console.log("value:" + x);
+          arr[element.key] = x;
+          arr['existe'] = "SI";
           //x["$key"] = element.key;
-          var zz = element.payload.toJSON();
           
+
           //this.productObj.push(x as Producto);
         });
         console.log(arr);
-        console.log(arr['serpro']);
-        
-        this.invoice.push(
-          {
-            id: 5,
-            codpro: "123",
-            nompro: "PANTALLA-HTC-DESIRE-100",
-            cantid: 1,
-            punituser: 10.25,
-            codcol: "000001",
-            descolor: "TRANSPARENTE",
-            talla: "000001",
-            serpro: arr['serpro'],
-            fecvendet: "2019-01-01"
+        console.log("sale:" + arr['codpro']);
+        if (arr['existe'] == 'SI') {
+          this.invoice.push(
+            {
+              id: 5,
+              codpro: arr['codpro'],
+              nompro: arr['nompro'],
+              cantid: 1,
+              punituser: arr['precio2'],
+              codcol: arr['codcol'],
+              descolor: arr['descolor'],
+              talla: arr['talla'],
+              serpro: arr['serpro'],
+              fecvendet: arr['fecvendet']
+            });
           }
-        )
-        ;
-        /*
-        this.productObj.id = 5;
-        this.productObj.serpro = arr['serpro'];
-        this.productObj.codpro = arr['codpro'];
-        this.productObj.nompro = "F A L T A";
-        this.productObj.codcol = arr['codcol'];
-        this.productObj.descolor = "F A L T A";
-        this.productObj.talla = "F A L T A";
-        this.productObj.fecvendet = arr['fecvendet'];
-        this.productObj.cantid = arr['cantid'];
-        this.productObj.punituser = 0.00;
-        
-        this.invoice.push(this.productObj as Producto);
-        */
-      });      
-      console.log("Lista: " + this.productObj);
+      
+    /*
+    this.productObj.id = 5;
+    this.productObj.serpro = arr['serpro'];
+    this.productObj.codpro = arr['codpro'];
+    this.productObj.nompro = "F A L T A";
+    this.productObj.codcol = arr['codcol'];
+    this.productObj.descolor = "F A L T A";
+    this.productObj.talla = "F A L T A";
+    this.productObj.fecvendet = arr['fecvendet'];
+    this.productObj.cantid = arr['cantid'];
+    this.productObj.punituser = 0.00;
+    
+    this.invoice.push(this.productObj as Producto);
+    */
+  });
+  console.log("Lista: " + this.productObj);
       
 
   
   }
+
+onSearchChange(searchValue: string) {
+  this.searchValue = searchValue;
+
+  console.log("searchValue = " + this.searchValue);
+
+}
 
 }
