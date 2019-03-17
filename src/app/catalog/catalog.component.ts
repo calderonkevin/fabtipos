e206e666386f8e6f0473df850d5fafce2b31b513
ocalderon <calderon_edsel@hotmail.com>
2019-03-17 18:11:20 -0500

    Buscador

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,45 +1,44 @@
-import { Component, OnInit , TemplateRef} from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 
 import { GLOBAL } from '../common/services/global';
 import { LoginService } from '../common/services/login.service';
 import { ProductService } from '../common/services/product.service';
-import { Router, ActivatedRoute, Params} from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 // model
 import { Product } from '../models/product';
 
-
-
 export interface Section {
-  name: string;  
-    
+  name: string;
+
 }
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css'],
-  providers: [LoginService ]
+  providers: [LoginService]
 })
 export class CatalogComponent implements OnInit {
 
   productList: Product[];
-  identity: any;  
+  identity: any;
   invoiceCab = [];
-  codError:number;
-  msgError:string;
-  status:string;
+  codError: number;
+  msgError: string;
+  status: string;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _loginService: LoginService,    
-    private _productService: ProductService,
+    private _loginService: LoginService,
+    private _productService: ProductService  
   ) {
   }
 
-
   ngOnInit() {
+    
+   // console.log(this.headerComponent.descuento);
     /*
     this._productService.getProduct()
       .snapshotChanges()
@@ -56,41 +55,7 @@ export class CatalogComponent implements OnInit {
       });
       */
 
-      console.log("entrando a validar");
-      this.codError = -999;
-      
-      this._loginService.articulos().subscribe(
-        response => {
-          console.log("L I S T A   D E   P R O D U C T O S");
-            console.log(response);
-            this.codError = response.code;
-            this.msgError = response.msg;
-            this.productList = response.data;
-            if(this.codError == 0){
-              
-                this.status = "success";               
-                
-            }else{
-                this.status = "danger";
-            }
-
-        },
-        error => {
-            console.log(<any>error);
-            //console.log("error 454545.");
-            var errorMessage = <any>error;
-            if (errorMessage != null){
-                var body = JSON.parse(error._body);
-                this.codError = -1;
-            }
-        }
-    )
-      
-
-
-
-
-        this.identity = this._loginService.getIdentity();    
+    this.identity = this._loginService.getIdentity();
     //this.invoiceCab = [{'codcli':'5','coddir':'0001','codper':'44001713','nomcom':'otros mas'}];
     this.invoiceCab = this._loginService.getDataDef();
     console.log("entra a oninit CATALOGO");
@@ -102,8 +67,6 @@ export class CatalogComponent implements OnInit {
     localStorage.clear();
     this._router.navigate(['/']);
   }
-
- 
 
   products: Section[] = [
     { name: 'Photos' },
@@ -197,4 +160,40 @@ export class CatalogComponent implements OnInit {
     { name: 'Photos' },
 
   ];
+
+  loadCatalogo(searchValue: string) {
+    console.log("loadCatalogo");
+    console.log(searchValue);
+  
+    this.codError = -999;
+    if (searchValue != "") {
+      this._loginService.articulos().subscribe(
+        response => {
+          console.log("L I S T A   D E   P R O D U C T O S");
+          console.log(response);
+          this.codError = response.code;
+          this.msgError = response.msg;
+          this.productList = response.data;
+          if (this.codError == 0) {
+
+            this.status = "success";
+
+          } else {
+            this.status = "danger";
+          }
+
+        },
+        error => {
+          console.log(<any>error);
+          //console.log("error 454545.");
+          var errorMessage = <any>error;
+          if (errorMessage != null) {
+            var body = JSON.parse(error._body);
+            this.codError = -1;
+          }
+        }
+      )
+    }
+
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,47 +1,41 @@
-import { Component , OnInit} from '@angular/core';
-import {MatDialog} from '@angular/material';
-
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { Category } from '../models/category';
-
 import { LoginService } from '../common/services/login.service';
 import { ProductService } from '../common/services/product.service';
-
-
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 //Dialog
-import {MyDialogComponent} from '../my-dialog/my-dialog.component';
+import { MyDialogComponent } from '../my-dialog/my-dialog.component';
+import { CatalogComponent } from '../catalog/catalog.component';
 
 declare var jQuery: any;
 declare var $: any;
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  providers: [LoginService ]
+  providers: [LoginService]
 })
-export class HeaderComponent implements OnInit  {
- 
-  
+export class HeaderComponent implements OnInit {
+
   categoriaList: Category[];
-  identity: any;  
-  datadef: any;  
-  descuento=0;
-  tipoDescuento=0;
+  identity: any;
+  datadef: any;
+  descuento = 0;
+  tipoDescuento = 0;
 
   constructor(
     private _loginService: LoginService,
     private _productService: ProductService,
     private _route: ActivatedRoute,
     private _router: Router,
-    public dialog: MatDialog
-  ) {    
+    public dialog: MatDialog,
+    private catalogComponent: CatalogComponent,
+  ) {
   }
 
-  
-  
   ngOnInit() {
 
     this._productService.getCategoria()
@@ -58,40 +52,42 @@ export class HeaderComponent implements OnInit  {
         });
       });
 
-      
-      this.identity = this._loginService.getIdentity();    
-      this.datadef = this._loginService.getDataDef()
+
+    this.identity = this._loginService.getIdentity();
+    this.datadef = this._loginService.getDataDef()
     //this.invoiceCab = [{'codcli':'5','coddir':'0001','codper':'44001713','nomcom':'otros mas'}];
     //this.invoiceCab = this._loginService.getDataDef();
     console.log("entra a oninit HEAD");
     console.log(this._loginService.getDataDef());
 
-   }
+  }
 
-   logout(): void {
+  logout(): void {
     localStorage.clear();
     this._router.navigate(['/']);
   }
 
   onSearchChange(searchValue: string) {
-    console.log("Ingresa con enter");
-    $("#txtBusqueda").select();
-    if(searchValue.startsWith("0"))
-    {
-     $("#detectabarra").val(searchValue);
-     $("#btnejecuta").click();
+    //console.log("Ingresa con enter");
+    //console.log(searchValue);
+
+    this.catalogComponent.loadCatalogo(searchValue);
+
+    /*$("#txtBusqueda").select();
+    if (searchValue.startsWith("0")) {
+      $("#detectabarra").val(searchValue);
+      $("#btnejecuta").click();
     }
-    else{
+    else {
       console.log("no hace nada")
     }
-    return;
+    return;*/
 
 
   }
 
   openDialog(): void {
-  
-    
+
     let dialogRefDes = this.dialog.open(MyDialogComponent, {
       width: '450px',
       // data: { name: this.name, animal: this.animal }
@@ -99,18 +95,18 @@ export class HeaderComponent implements OnInit  {
       data: {
         descuento: this.descuento,
         tipoDescuento: this.tipoDescuento,
-        
+
       }
     });
 
     dialogRefDes.afterClosed().subscribe(result => {
-        //alert(result);
-       
-        //console.log('The dialog was closed');
-        //this.animal = result;
+      //alert(result);
+
+      //console.log('The dialog was closed');
+      //this.animal = result;
     });
   }
 
-  
+
 
 }
