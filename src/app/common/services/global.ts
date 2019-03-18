7abc7f997d925c68a4542a83022b11ab85977c55
calderonkevin <calderon_kevin@yahoo.es>
2019-03-17 22:26:11 -0500

    url actualizada

--- src/app/common/services/global.ts
+++ src/app/common/services/global.ts
@@ -1,6 +1,7 @@
 export var GLOBAL = {
     url: 'http://fabti.pe/api/',
-    //url: 'http://sisgenlab.com/api/',
     url2: 'http://fabti.pe/rest/',
+    //url: 'http://sisgenlab.com/api/',
+    //url2: 'http://sisgenlab.com/rest/',
 
 }
